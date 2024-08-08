bdf19e1373641f61661b9725a5b8ab75b7527194
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2024-08-08 23:30:25 +0700

    task 2: XML

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtAuthGuard } from 'src/auth/guard';
+import { XmlParserModule } from 'src/xml-parser/xml-parser.module';
 import { AuthModule } from '../auth/auth.module';
 import { XmlParserService } from '../xml-parser/xml-parser.service';
 import { BookingController } from './booking.controller';
-import { XmlParserModule } from 'src/xml-parser/xml-parser.module';
 
 @Module({
   imports: [
